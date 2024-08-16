ab5334cc7a7880e82322d6e04b0391c2db6298c7
mostafa esmail <mostafaelshrbeeny98@gmail.com>
2024-08-16 04:02:46 +0300

    create project and add folder and app styles

---
+++ android/app/src/main/kotlin/com/example/note/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.note
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/core/app_styles.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class AppStyles {
+  static const TextStyle styleRegular12 = TextStyle(
+    fontSize: 12,
+    fontFamily: "PTSans",
+    fontWeight: FontWeight.w400,
+    color: Colors.black,
+  );
+  static const TextStyle styleRegular18 = TextStyle(
+    fontSize: 18,
+    fontFamily: "PTSans",
+    fontWeight: FontWeight.w400,
+    color: Colors.black,
+  );
+  static const TextStyle styleBold14 = TextStyle(
+    fontSize: 14,
+    fontFamily: "PTSans",
+    fontWeight: FontWeight.w700,
+    color: Colors.black,
+  );
+  static const TextStyle styleBold24 = TextStyle(
+    fontSize: 24,
+    fontFamily: "PTSans",
+    fontWeight: FontWeight.w700,
+    color: Colors.black,
+  );
+  static const TextStyle styleBold12 = TextStyle(
+    fontSize: 12,
+    fontFamily: "PTSans",
+    fontWeight: FontWeight.w700,
+    color: Colors.white,
+  );
+  static const TextStyle styleRegular16 = TextStyle(
+    fontSize: 16,
+    fontFamily: "PTSans",
+    fontWeight: FontWeight.w400,
+    color: Colors.black,
+  );
+  static const TextStyle styleRegular14 = TextStyle(
+    fontSize: 14,
+    fontFamily: "PTSans",
+    fontWeight: FontWeight.w400,
+    color: Colors.black,
+  );
+}
